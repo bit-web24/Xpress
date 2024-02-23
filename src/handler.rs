e536452b0afe66883ca74a1c935256db71485042
bit-web24 <bitweb24@gmail.com>
2024-02-23 23:05:01 +0530

    parse http req seperately

--- src/handler.rs
+++ src/handler.rs
@@ -19,50 +19,16 @@ impl RequestHandler {
     }
 
     pub async fn handle(&mut self, mut socket: TcpStream) -> Result<()> {
-        // Handler
         let mut buf = [0; 1024];
-        let n = socket
-            .read(&mut buf)
-            .await
-            .expect("Error reading from the socket!");
-
-        let request_str = String::from_utf8_lossy(&buf[..n]);
-        let mut lines = request_str.lines();
-
-        // Extract request line
-        let req_ln = lines.next().unwrap();
-        let mut request_line = req_ln.split_whitespace();
-
-        let (method, path, version) = (
-            Method::from(request_line.next().unwrap()),
-            request_line.next().unwrap(),
-            request_line.next().unwrap(),
-        );
-
-        // Extract headers
-        let mut headers = Vec::new();
-        while let Some(line) = lines.next() {
-            if line.trim().is_empty() {
-                break; // Empty line indicates end of headers
-            }
-            headers.push(line.to_string());
-        }
-
-        // Extract data (if any)
-        let data = lines.collect::<Vec<&str>>().join("\n");
-
-        let req = Request::new(
-            (method.clone(), path.to_string(), version.to_string()),
-            headers,
-            data,
-        );
-
+        let n = socket.read(&mut buf).await?;
+        let mut request_str = String::from_utf8_lossy(&buf[..n]);
+        let req = Request::parse(&mut request_str);
         let res = Response::new(socket);
 
-        match method {
+        match req.method {
             Method::Get => {
                 let mut routes = self.routes.lock().await;
-                match routes.get(path) {
+                match routes.get(&req.path) {
                     Some(ref route) => (route.callback)(req, res).await,
                     None => Err(Error::new(
                         std::io::ErrorKind::NotFound,
--- src/router/request.rs
+++ src/router/request.rs
@@ -9,14 +9,44 @@ pub struct Request {
 }
 
 impl Request {
-    pub fn new(req_ln: (Method, String, String), headers: Vec<String>, body: String) -> Self {
-        let (method, path, version) = req_ln;
+    pub fn new() -> Self {
         Self {
-            method,
-            path,
-            version,
-            headers: Header::from(headers),
-            body: Some(body),
+            method: Method::Undefined,
+            path: "".to_string(),
+            version: "".to_string(),
+            headers: Header::new(),
+            body: None,
         }
+    }
+
+    pub fn parse(request_str: &mut std::borrow::Cow<'_, str>) -> Self {
+        let mut req = Self::new();
+
+        let mut lines = request_str.lines();
+
+        // Extract request line
+        let req_ln = lines.next().unwrap();
+        let mut request_line = req_ln.split_whitespace();
+
+        req.method = Method::from(request_line.next().unwrap());
+        req.path = request_line.next().unwrap().to_string();
+        req.version = request_line.next().unwrap().to_string();
+
+        // Extract headers
+        let mut headers = Vec::new();
+        while let Some(line) = lines.next() {
+            if line.trim().is_empty() {
+                break;
+            }
+            headers.push(line.to_string());
+        }
+
+        req.headers = Header::from(headers);
+
+        // Extract data (if any)
+        let data = lines.collect::<Vec<&str>>().join("\n");
+        req.body = if data.is_empty() { None } else { Some(data) };
+
+        req
     }
 }
