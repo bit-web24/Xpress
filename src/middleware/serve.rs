aeb6651f256f7923c43923c7c86a2d028a15448c
bit-web24 <bitweb24@gmail.com>
2024-02-26 23:41:41 +0530

    re structure serve.rs

--- src/middleware/serve.rs
+++ src/middleware/serve.rs
@@ -1,3 +1,19 @@
-mod serve_static;
+use crate::{middleware::Middleware, router::request::Request};
 
-pub use serve_static::ServeStatic;
+pub struct ServeStatic {
+    root: String,
+}
+
+impl ServeStatic {
+    pub fn dir(root: &str) -> Self {
+        Self {
+            root: root.to_string(),
+        }
+    }
+}
+
+impl Middleware for ServeStatic {
+    fn handle(&self, req: &mut Request) -> std::io::Result<()> {
+        Ok(())
+    }
+}
